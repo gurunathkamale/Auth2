9917e3c2d703c83cb00e34c80da8dc11ba316702
gurunathkamale <ramgurunath7@gmail.com>
2025-12-05 10:44:21 +0530

    backend chnages

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -7,3 +7,4 @@ export const PORT = process.env.PORT || "";
 // export  const MONGO_URI = process.env.MONGO_RRI || "";
 export const JWT_SECRET = process.env.JWT_SECRET || "default";
 export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
+export const VERSEL_URL = process.env.VERCEL_URL
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 
 import express from 'express';
 import cors from 'cors'
-import { CLIENT_URL, PORT } from './config/env';
+import { CLIENT_URL, PORT, VERSEL_URL } from './config/env';
 import dbConnect from './db/dbConnection';
 import cookieParser from 'cookie-parser';
 import authRoutes from './routes/authRoutes'
@@ -10,10 +10,20 @@ import authRoutes from './routes/authRoutes'
 const app = express();
 app.use(express.json());
 app.use(cookieParser())
-app.use(cors({
-    origin: CLIENT_URL,
+// app.use(cors({
+//     origin: "https://auth2-48zs.vercel.app/",
+//     credentials: true
+// }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://auth2-48zs.vercel.app"
+    ],
     credentials: true
-}));
+  })
+);
+
 
 // dotenv.config()
 app.get('/', (_, res)=>res.send('API Running.....'))
